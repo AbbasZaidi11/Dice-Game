21e226d1250e99e72d75f5b389706da7b3c2814c
AbbasZaidi11 <105381187+AbbasZaidi11@users.noreply.github.com>
2022-06-29 22:22:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelector(".img1");
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource2 = "images/" + randomDiceImage2; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelector(".img2");
+
+image1.setAttribute("src", randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+  document. querySelector("h1").innerHTML="🚩Player 1 wins";
+}
+else if (randomNumber2>randomNumber1) {
+  document.querySelector("h1").innerHTML="Player 2 wins🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
